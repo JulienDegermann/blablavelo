d79bca88c07c16233e98a4f135e0da257da72383
JulienDegermann <degermann.julien@gmail.com>
2024-05-05 14:35:08 +0200

    update comment into string field

--- src/Entity/RideComment.php
+++ src/Entity/RideComment.php
@@ -7,6 +7,8 @@ use App\Repository\RideCommentRepository;
 use App\Traits\Entity\DatesTrait;
 use App\Traits\Entity\TextTrait;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 #[ORM\Entity(repositoryClass: RideCommentRepository::class)]
 class RideComment
@@ -14,8 +16,29 @@ class RideComment
 
     use DatesTrait;
     use IdTrait;
-    use TextTrait;
 
+    #[ORM\Column(length: 255)]
+    #[Assert\Sequentially([
+      new Assert\NotBlank(
+        message: 'Ce champ est obligatoire.'
+      ),
+      new Assert\Type(
+        type: 'string',
+        message: 'Ce champ doit être une chaine de caractères.'
+      ),
+      new Assert\Length(
+        min: 2,
+        max: 255,
+        minMessage: 'Ce champ doit contenir au moins {{ limit }} caractères.',
+        maxMessage: 'Ce champ doit contenir au maximum {{ limit }} caractères.',
+      ),
+      new Assert\Regex(
+        pattern: '/^(?![×Þß÷þø])[0-9a-zA-ZÀ-ÿ\-\s,?\':!()]{2,255}$/u',
+        message: 'Le message contient des caractères non autorisés.'
+      )
+    ])]
+    private ?string $text = null;
+  
     #[ORM\ManyToOne(inversedBy: 'rideComments')]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $author = null;
@@ -31,15 +54,16 @@ class RideComment
 
     public function getText(): ?string
     {
-        return $this->text;
+      return $this->text;
     }
-
+  
     public function setText(string $text): static
     {
-        $this->text = $text;
-
-        return $this;
+      $this->text = $text;
+  
+      return $this;
     }
+
 
     public function getAuthor(): ?User
     {
