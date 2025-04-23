fe4715f803fca544ef36a36221168617153eb35f
Julien Degermann <julien@MacBookAir-001.lan>
2025-04-23 17:32:05 +0200

    fix trophees display

--- src/Application/Controller/InfoController.php
+++ src/Application/Controller/InfoController.php
@@ -3,15 +3,20 @@
 namespace App\Application\Controller;
 
 // dependencies
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Domain\User\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 // entities
-use App\Domain\User\User;
+use App\Domain\Ride\Contrat\FindMyRidesInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class InfoController extends AbstractController
 {
+    public function __construct(
+        private readonly FindMyRidesInterface        $findMyRides
+    ) {}
+
     #[Route('/comment-ca-marche', name: 'app_info')]
     public function index(
     ): Response
@@ -21,6 +26,9 @@ class InfoController extends AbstractController
 
         return $this->render('info/index.html.twig', [
             'user' => $user,
+            'my_next_rides' => $myRides['myNextRides'],
+            'my_created_rides' => $myRides['myCreatedRides'],
+            'my_prev_rides' => $myRides['allMyRides'],
         ]);
     }
 }
--- src/Application/Controller/LegalNoticeController.php
+++ src/Application/Controller/LegalNoticeController.php
@@ -3,24 +3,37 @@
 namespace App\Application\Controller;
 
 // dependencies
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Domain\User\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 // entities
-use App\Domain\User\User;
+use App\Domain\Ride\Contrat\FindMyRidesInterface;
+use App\Domain\User\Contrat\UpdateUserSettingsInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LegalNoticeController extends AbstractController
 {
+
+    public function __construct(
+        private readonly FindMyRidesInterface        $findMyRides,
+    ) {}
+
     #[Route('/mentions-legales', name: 'app_legal_notice')]
-    public function index(
-    ): Response
+    public function index(): Response
     {
+
         /** @var User $user */
         $user = $this->getUser();
 
+        $myRides = ($this->findMyRides)($user);
+
+
         return $this->render('legal_notice/index.html.twig', [
             'user' => $user,
+            'my_next_rides' => $myRides['myNextRides'],
+            'my_created_rides' => $myRides['myCreatedRides'],
+            'my_prev_rides' => $myRides['allMyRides'],
         ]);
     }
 }
